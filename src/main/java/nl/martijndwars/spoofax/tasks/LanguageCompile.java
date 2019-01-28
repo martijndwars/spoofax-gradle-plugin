64bee649d865bc62d4acd2984e64d75b239d61e2
Martijn Dwars <martijn.dwars@oracle.com>
2019-01-28 09:32:08 +0100

    Make LanguageCompile incremental

--- src/main/java/nl/martijndwars/spoofax/tasks/LanguageCompile.java
+++ src/main/java/nl/martijndwars/spoofax/tasks/LanguageCompile.java
@@ -1,12 +1,18 @@
 package nl.martijndwars.spoofax.tasks;
 
+import com.google.common.collect.Lists;
 import nl.martijndwars.spoofax.SpoofaxOverrides;
 import nl.martijndwars.spoofax.SpoofaxPlugin;
+import org.apache.commons.vfs2.FileObject;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.gradle.api.internal.AbstractTask;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
+import org.metaborg.core.MetaborgConstants;
 import org.metaborg.core.MetaborgException;
 import org.metaborg.core.action.CompileGoal;
 import org.metaborg.core.build.BuildInput;
@@ -14,6 +20,7 @@ import org.metaborg.core.build.BuildInputBuilder;
 import org.metaborg.core.messages.StreamMessagePrinter;
 import org.metaborg.spoofax.core.resource.SpoofaxIgnoresSelector;
 import org.metaborg.spoofax.meta.core.build.LanguageSpecBuildInput;
+import org.metaborg.spoofax.meta.core.build.SpoofaxLangSpecCommonPaths;
 import org.metaborg.spoofax.meta.core.project.ISpoofaxLanguageSpec;
 import org.metaborg.util.log.ILogger;
 import org.metaborg.util.log.LoggerUtils;
@@ -28,6 +35,7 @@ import static nl.martijndwars.spoofax.SpoofaxInit.*;
 
 public class LanguageCompile extends AbstractTask {
   private static final ILogger logger = LoggerUtils.logger(LanguageCompile.class);
+  public static final String GENERATED_EDITOR_ESV = "target/metaborg/editor.esv.af";
   public static final String CTREE_PROVIDER = "target/metaborg/stratego.ctree";
   public static final String JAR_PROVIDER = "target/metaborg/stratego.jar";
 
@@ -54,6 +62,32 @@ public class LanguageCompile extends AbstractTask {
   @Input
   public ListProperty<String> getOverrides() {
     return overrides;
+  }
+
+  // TODO: Ideally we'd use the languagePathService here, but this requires the dependencies (e.g. esv) to be loaded, which is not the case.
+  @InputFiles
+  public Iterable<File> getInputFiles() {
+    ISpoofaxLanguageSpec languageSpec = languageSpec(getProject());
+    SpoofaxLangSpecCommonPaths paths = new SpoofaxLangSpecCommonPaths(languageSpec.location());
+
+    // TODO: This list is incomplete
+    return Lists.newArrayList(
+      toFile(paths.transDir()),
+      getProject().file(MetaborgConstants.FILE_CONFIG)
+    );
+  }
+
+  @OutputDirectory
+  public File getOutputDirectory() {
+    ISpoofaxLanguageSpec languageSpec = languageSpec(getProject());
+    SpoofaxLangSpecCommonPaths paths = new SpoofaxLangSpecCommonPaths(languageSpec.location());
+
+    // TODO: This directory is too general, because LanguageArchive will put the .spoofax-language in here, invalidating the output.
+    return toFile(paths.targetDir());
+  }
+
+  protected File toFile(@Nullable FileObject fileObject) {
+    return getSpoofax(getProject()).resourceService.localFile(fileObject);
   }
 
   @TaskAction
@@ -90,7 +124,7 @@ public class LanguageCompile extends AbstractTask {
   }
 
   protected void fixPackedEsv() {
-    File file = getProject().file("target/metaborg/editor.esv.af");
+    File file = getProject().file(GENERATED_EDITOR_ESV);
     Path path = file.toPath();
 
     if (!file.exists()) {
